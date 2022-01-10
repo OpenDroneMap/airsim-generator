1569a6c4197ddba021af4116efda26bde8ad0057
Piero Toffanin <pt@masseranolabs.com>
2022-01-10 11:31:13 -0500

    Fix round geotransforms

--- clip.py
+++ clip.py
@@ -1,6 +1,8 @@
 from osgeo import gdal
 from osgeo.gdalconst import GA_ReadOnly
 import argparse
+import rasterio
+from rasterio.windows import get_data_window
 import subprocess
 from pipes import quote
 
@@ -27,3 +29,9 @@ miny = maxy + geoTransform[5] * data.RasterYSize
 cmd = 'gdal_translate -projwin ' + ' '.join([str(x) for x in [minx, maxy, maxx, miny]]) + ' -a_ullr ' + ' '.join([str(x) for x in [minx, maxy, maxx, miny]]) + ' -of GTiff ' + quote(args.input) + " " + quote(args.output)
 print(cmd)
 subprocess.check_output(cmd, shell=True)
+
+# Make sure the geotransforms are exactly the same,
+# as gdal_translate sometimes rounds res values 
+with rasterio.open(args.output, 'r+') as out_raster:
+    with rasterio.open(args.mask, 'r') as in_mask:
+        out_raster.transform = in_mask.transform
