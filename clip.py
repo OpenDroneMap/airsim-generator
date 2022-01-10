84365cee8d9c6fd6bbb65d102d45babf62e99ff8
Piero Toffanin <pt@masseranolabs.com>
2022-01-10 10:24:08 -0500

    Add clip

---
+++ clip.py
@@ -0,0 +1,26 @@
+from osgeo import gdal
+from osgeo.gdalconst import GA_ReadOnly
+import argparse
+import subprocess
+from pipes import quote
+
+parser = argparse.ArgumentParser(description='Clip rasters (with the same CRS)')
+parser.add_argument('input',
+                type=str,
+                help='Input raster')
+parser.add_argument('mask',
+                type=str,
+                help='Mask raster')
+parser.add_argument('output',
+                type=str,
+                default="clipped.tif",
+                help='Output raster')
+args = parser.parse_args()
+
+data = gdal.Open(args.mask, GA_ReadOnly)
+geoTransform = data.GetGeoTransform()
+minx = geoTransform[0]
+maxy = geoTransform[3]
+maxx = minx + geoTransform[1] * data.RasterXSize
+miny = maxy + geoTransform[5] * data.RasterYSize
+subprocess.check_output('gdal_translate -projwin ' + ' '.join([str(x) for x in [minx, maxy, maxx, miny]]) + ' -of GTiff ' + quote(args.input) + " " + quote(args.output), shell=True)
